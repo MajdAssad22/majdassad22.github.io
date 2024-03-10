42964314d4f40f84175b1ab39e175b5620d05fbe
MajdAssad22 <83403902+MajdAssad22@users.noreply.github.com>
2024-03-10 18:17:18 -0400

    Added routing and needed pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
+// React
+import React, { useLayoutEffect, useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+
+// CSS & Media
 import './App.css';
 
+// Pages
+import HomePage from './pages/home-page';
+import AboutPage from './pages/about-page';
+import ContactPage from './pages/contact-page';
+
+// Components
+import Loader from './components/loader';
+import MainLayout from './components/main-layout';
+import PortfolioPage from './pages/portfolio-page';
+
+const Wrapper = ({ children }) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children
+}
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
-    <div>
-      <header className='h-screen flex items-center justify-center bg-dark'>
-        <h1 className='p-10 text-6xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-t from-dark to-light scale-90 hover:scale-100 transition-all ease-in-out duration-300'>Coming Soon!</h1>
-      </header>
+    <div className='bg-dark'>
+      {loading ? (
+        <Loader isScreenLoader={true}/>
+      ) : (
+        <BrowserRouter>
+          <Wrapper>
+            <Routes>
+              <Route path='/' element={<MainLayout/>}>
+                <Route index element={<HomePage />} />
+              </Route>
+              <Route path='/about' element={<MainLayout/>}>
+                <Route index element={<AboutPage />} />
+              </Route>
+              <Route path='/contact' element={<MainLayout/>}>
+                <Route index element={<ContactPage />} />
+              </Route>
+              <Route path='/portfolio' element={<MainLayout/>}>
+                <Route index element={<PortfolioPage />} />
+              </Route>
+            </Routes>
+          </Wrapper>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
---
+++ src/pages/home-page.js
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="p-10 text-6xl font-bold text-center text-light scale-90 hover:scale-100 hover:text-dark transition-all ease-in-out duration-300">
+        Coming Soon!
+      </h1>
+    </div>
+  );
+};
+
+export default HomePage;
