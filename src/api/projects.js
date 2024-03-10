7965ed5a255c34fdda1b84a4747881028bb141c3
MajdAssad22 <83403902+MajdAssad22@users.noreply.github.com>
2024-03-10 18:05:37 -0400

    Added the api for the projects

---
+++ src/api/projects.js
@@ -0,0 +1,8 @@
+const getProjects = async () => {
+    const response = await fetch(process.env.REACT_APP_GITHUB_API + "majdassad22/repos");
+    const projects = await response.json();
+    console.log(projects);
+    return projects;
+}
+
+export default getProjects;
